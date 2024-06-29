2d5c5627606a859bbf848614c5504d77d4cf4208
Birhan-Ayinalem <ayichewbirhanayinalem.com>
2024-06-30 01:00:57 +0300

    banner

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -4,7 +4,7 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRatedMovies: `/discover/movie/top_rated?api_key=${API_KEY}&language-en-US`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language-en-US`,
     fetchActionMovies: `/discover/movie?aip_key=${API_KEY}&with_geners=28`,
     fetchComedyMovies: `/discover/movie?aip_key=${API_KEY}&with_geners=35`,
     fetchHorrorMovies: `/discover/movie?aip_key=${API_KEY}&with_geners=27`,
