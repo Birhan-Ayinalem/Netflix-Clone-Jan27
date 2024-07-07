77a4a3022afd9259c0637dafe4f2d347df6e0d97
Birhan-Ayinalem <ayichewbirhanayinalem.com>
2024-07-08 01:38:12 +0300

    rows

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -5,11 +5,11 @@ const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language-en-US`,
-    fetchActionMovies: `/discover/movie?aip_key=${API_KEY}&with_geners=28`,
-    fetchComedyMovies: `/discover/movie?aip_key=${API_KEY}&with_geners=35`,
-    fetchHorrorMovies: `/discover/movie?aip_key=${API_KEY}&with_geners=27`,
-    fetchRomanceMovies: `/discover/movie?aip_key=${API_KEY}&with_geners=10749`,
-    fetchDocumentaries: `/discover/movie?aip_key=${API_KEY}&with_geners=99`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
     fetchTvShows: `tv/popular?api_key=${API_KEY}&language-en-US&page=1`
 }
 
